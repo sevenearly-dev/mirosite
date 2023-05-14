59cb107d2d8dd0087e5a2f6e2acdd2729846cf7a
sevenearly-dev <ssshi@fillume.com>
2023-05-15 06:53:50 +0800

    fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -21,11 +21,8 @@ export default function App({ Component, pageProps }: AppProps) {
     <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
       <Head>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3141436948416146"
-          crossOrigin="anonymous"
-        ></script>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3141436948416146"
+     crossorigin="anonymous"></script>
       </Head>
       {isDevelopment && isSocket && <ClientReload />}
       <Analytics />
